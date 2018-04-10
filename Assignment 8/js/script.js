eb2af4abf2f6d7d32d056540b30d0a6c52795b5b
Unknown <myersm6@gmatc.matc.edu>
2018-04-09 21:05:41 -0500

    Assignment 8 done

--- Assignment 8/js/script.js
+++ Assignment 8/js/script.js
@@ -78,4 +78,58 @@ function initPosts(){
   }
 }
 
+
+var messages = [];
+
+function Message(type, user, text) {
+  this.type = type;
+  this.user = user;
+  this.text = text;
+}
+
+function addMessage(event) {
+  var user, type, classname;
+  var messageInput = document.getElementById('message-input');
+  var messagesContainerEl = document.getElementById('message-container');
+
+  //determin message type based on button clicked
+  switch (event.target.id) {
+    case 'send-button':
+      user = 'Matt';
+      type = 'out';
+      className = 'out-message';
+      break;
+    default:
+      user = 'admin';
+      type = 'error';
+      className = 'error-message';
+  }
+
+  if(messageInput.value != ''){
+    //construct message and add to message collection
+
+    var message = new Message(type, user, messageInput.value);
+    messages.push(message);
+
+    // create element.
+    var messageText = document.createTextNode(message.text);
+    var messageEl = document.createElement('div');
+    messageEl.appendChild(messageText);
+    messageEl.className = className;
+
+    //add to DOM
+    messagesContainerEl.appendChild(messageEl);
+
+    //reset input
+    messageInput.value = '';
+  }
+}
+
+var init = function() {
+  //wire event handlers
+  document.getElementById('send-button').onclick = addMessage;
+}
+
+init();
+
 initPosts();
